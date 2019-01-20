3513f0317aa86f7113bb86d28077c6c7829c3b4e
Dmitry <ribkatt@gmail.com>
2019-01-20 17:23:52 +0000

    handle request's body

--- src/api/prettier.js
+++ src/api/prettier.js
@@ -2,7 +2,31 @@
 
 import prettier from 'prettier/standalone.js'
 
-export default (req, res) => {
+function processRequest(request) {
+  return new Promise((rs, rj) => {
+    let body = [];
+    request.on('error', (err) => {
+      console.error(err);
+      rj(err)
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      const result = Buffer.concat(body).toString();
+      console.log('result', result)
+      try {
+        rs(
+          JSON.parse(result)
+        );
+      } catch (err) {
+        console.error(err);
+        rj(err)
+      }
+    })
+  })
+}
+
+export default async (req, res) => {
+  const startTime = Date.now()
   const configDefaults = {
     trailingComma: "es5",
     tabWidth: 2,
@@ -11,11 +35,16 @@ export default (req, res) => {
     bracketSpacing: false,
     arrowParens: 'always',
   }
-  // res.setHeader('Content-Type', 'application/json')
-  const code = req.body.code || '"no code!"'
-  const config = Object.assign({}, configDefaults, req.body.config || {})
+  const bodyRaw = await processRequest(req)
+  const body = Object.assign({}, {
+    code: '"no code!"',
+    config: {},
+  }, bodyRaw)
+  const code = body.code
+  const config = Object.assign({}, configDefaults, body.config)
   const result = prettier.format(code, config)
-  const response = {code: result, config}
-  res.json(response, 2)
-  //res.end(`Hello!\nprettier keys: [${Object.keys(prettier).join(',')}]`)
+  const endTime = Date.now() - startTime
+  const response = { code: result, config, processTime: endTime }
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(response, null, 2))
 }
