fa37f22d592897d3be822b9338601a788e2b8706
Dmitry <ribkatt@gmail.com>
2019-01-20 19:15:39 +0000

    add head tags

--- src/www/pages/example.js
+++ src/www/pages/example.js
@@ -1,8 +1,10 @@
 import fetch from 'cross-fetch'
 import * as React from "react";
+import Head from 'next/head'
 import Link from "next/link";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import icon from '../icon.png'
 
 export default class ExamplePage extends React.Component {
   static getInitialProps() {
@@ -13,6 +15,13 @@ export default class ExamplePage extends React.Component {
   render() {
     return (
       <main>
+        <Head>
+          <meta charset="utf-8"/>
+          <title>API example</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+          <link rel="icon" href={icon}/>
+          <link rel="apple-touch-icon" href={icon}/>
+        </Head>
         <Header />
         <section>
           POST /api
--- src/www/pages/index.js
+++ src/www/pages/index.js
@@ -1,17 +1,26 @@
 import fetch from 'cross-fetch'
 
 import Link from "next/link";
+import Head from 'next/head'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import icon from '../icon.png'
 
 export default () => (
   <main>
+    <Head>
+      <meta charset="utf-8"/>
+      <title>Serverless Prettier</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+      <link rel="icon" href={icon}/>
+      <link rel="apple-touch-icon" href={icon}/>
+    </Head>
     <Header />
     <section>
       <Link href="/example">
-        <a>API request  example</a>
+        <a>API request example</a>
       </Link>
     </section>
-    <Footer/>
+    <Footer />
   </main>
 );
