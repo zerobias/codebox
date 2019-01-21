7c515339341a37190d963a03e9d0a4341d1ebe70
Dmitry <ribkatt@gmail.com>
2019-01-21 01:10:51 +0000

    separate common microservice code

--- api/format.js
+++ api/format.js
@@ -4,54 +4,17 @@ import prettier from 'prettier/standalone.js'
 import prettierConfig from './defaultPrettierConfig'
 import plugins from './plugins'
 
-function processRequest(request) {
-  return new Promise((rs, rj) => {
-    let body = [];
-    request
-      .on('error', (err) => {
-        rj(err)
-      })
-      .on('data', (chunk) => {
-        body.push(chunk);
-      })
-      .on('end', () => {
-        const result = Buffer.concat(body).toString();
-        console.log('request body', result)
-        try {
-          rs(
-            JSON.parse(result)
-          )
-        } catch (err) {
-          rj(err)
-        }
-      })
-  })
-}
-
-export default {
-  async task({startTime, req}) {
-    const bodyRaw = await processRequest(req)
-    const body = Object.assign({}, {
-      code: '"no code!"',
-      config: {},
-    }, bodyRaw)
-    const code = body.code
-    const config = Object.assign({}, prettierConfig, body.config, {plugins})
-    const result = prettier.format(code, config)
-    const configWithoutPlugins = Object.assign({}, config, {plugins: null})
-    const response = {
-      code: result,
-      config: configWithoutPlugins,
-      processTime: Date.now() - startTime,
-      success: true,
-    }
-    return response
-  },
-  onError({startTime}, error) {
-    return {
-      error,
-      success: false,
-      processTime: Date.now() - startTime,
-    }
+export default async function task(ctx, bodyRaw) {
+  const body = Object.assign({}, {
+    code: '"no code!"',
+    config: {},
+  }, bodyRaw)
+  const code = body.code
+  const config = Object.assign({}, prettierConfig, body.config, {plugins})
+  const result = prettier.format(code, config)
+  const configWithoutPlugins = Object.assign({}, config, {plugins: null})
+  return {
+    code: result,
+    config: configWithoutPlugins,
   }
 }
--- api/prettier.js
+++ api/prettier.js
@@ -1,33 +1,8 @@
-//@flow
 
 import format from './format'
+import taskService from '../lib/taskService'
 
-function stringifyError(err) {
-  return String(
-    err.message ||
-    err.code ||
-    err.description ||
-    err.name ||
-    'Error'
-  )
-}
-async function trycatch(ctx, {task, onError}) {
-  let result
-  try {
-    result = await task(ctx)
-  } catch (err) {
-    const message = stringifyError(err)
-    console.error(message)
-    result = onError(ctx, message)
-  }
-  return result
-}
-
-export default async (req, res) => {
-  const response = await trycatch(
-    {startTime: Date.now(), req},
-    format
-  )
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(response, null, 2))
-}
+export default taskService({
+  serviceName: 'prettier',
+  task: format,
+})
