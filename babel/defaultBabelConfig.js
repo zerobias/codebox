dc95ad320ea21011055c4348e92c2abd86ccc363
Dmitry <ribkatt@gmail.com>
2019-01-23 19:06:18 +0000

    use  shorthands for babel config

--- babel/defaultBabelConfig.js
+++ babel/defaultBabelConfig.js
@@ -3,31 +3,31 @@
 export default {
   babelrc: false,
   presets: [
-    '@babel/preset-flow',
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        useBuiltIns: false,
-        modules: false,
-        shippedProposals: true,
-        targets: {
-          node: '10',
-          browsers: [
-            'last 2 Chrome versions',
-            'last 2 Firefox versions',
-            'last 2 Safari versions',
-            'last 2 Edge versions',
-          ],
-        },
-      },
-    ],
+    'flow',
+    'react',
+    // [
+    //   '@babel/preset-env',
+    //   {
+    //     loose: true,
+    //     useBuiltIns: false,
+    //     modules: false,
+    //     shippedProposals: true,
+    //     targets: {
+    //       node: '10',
+    //       browsers: [
+    //         'last 2 Chrome versions',
+    //         'last 2 Firefox versions',
+    //         'last 2 Safari versions',
+    //         'last 2 Edge versions',
+    //       ],
+    //     },
+    //   },
+    // ],
   ],
   plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    'proposal-export-namespace-from',
+    'proposal-optional-chaining',
+    'proposal-nullish-coalescing-operator',
+    ['proposal-class-properties', {loose: true}],
   ]
 }
