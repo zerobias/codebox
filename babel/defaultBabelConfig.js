fe81e4f02e3a6813f536ce83d38c18910d4b450c
Dmitry <ribkatt@gmail.com>
2019-01-23 18:11:40 +0000

    add babel api

---
+++ babel/defaultBabelConfig.js
@@ -0,0 +1,33 @@
+//@flow
+
+export default {
+  babelrc: false,
+  presets: [
+    '@babel/preset-flow',
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        useBuiltIns: false,
+        modules: false,
+        shippedProposals: true,
+        targets: {
+          node: '10',
+          browsers: [
+            'last 2 Chrome versions',
+            'last 2 Firefox versions',
+            'last 2 Safari versions',
+            'last 2 Edge versions',
+          ],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+  ]
+}
---
+++ babel/service.js
@@ -0,0 +1,23 @@
+
+import {transformAsync} from "@babel/core"
+
+import taskService from '../lib/taskService'
+import babelConfig from './defaultBabelConfig'
+
+export default taskService({
+  serviceName: 'babel',
+  async task(ctx, bodyRaw) {
+    const body = Object.assign({}, {
+      code: '"no code!"',
+      config: {},
+    }, bodyRaw)
+    const code = body.code
+    const config = Object.assign({}, babelConfig, body.config)
+
+    const result = await transformAsync(code, config)
+    return {
+      code: result.code,
+      config,
+    }
+  },
+})
