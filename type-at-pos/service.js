fd554dbef7a783056d42c68844f6cb2c8b12c003
Dmitry <ribkatt@gmail.com>
2019-04-04 15:17:26 +0000

    register libs in flow & type-at-pos

--- type-at-pos/service.js
+++ type-at-pos/service.js
@@ -2,6 +2,49 @@
 import flow from '@goodmind/flow-js'
 import taskService from '../lib/taskService'
 
+import {
+  bom,
+  core,
+  cssom,
+  dom,
+  indexeddb,
+  intl,
+  node,
+  react,
+  streams,
+} from './typedef'
+
+let start
+try {
+  console.log('start flow')
+  start = Date.now()
+  flow.registerFile('/static/bom.js', bom)
+  flow.registerFile('/static/core.js', core)
+  flow.registerFile('/static/cssom.js', cssom)
+  flow.registerFile('/static/dom.js', dom)
+  flow.registerFile('/static/indexeddb.js', indexeddb)
+  flow.registerFile('/static/intl.js', intl)
+  flow.registerFile('/static/node.js', node)
+  flow.registerFile('/static/react.js', react)
+  flow.registerFile('/static/streams.js', streams)
+
+  flow.setLibs([
+    '/static/bom.js',
+    '/static/core.js',
+    '/static/cssom.js',
+    '/static/dom.js',
+    '/static/indexeddb.js',
+    '/static/intl.js',
+    '/static/node.js',
+    '/static/react.js',
+    '/static/streams.js',
+  ])
+} catch (err) {
+  console.error(err)
+} finally {
+  console.log('flow registered', Date.now() - start)
+}
+
 function typeAtPos({filename, body, line, col}) {
   return flow.typeAtPos(
     filename,
