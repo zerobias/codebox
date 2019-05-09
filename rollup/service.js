15377c8ba2ebc63ea3a68737198377b08669a4ea
Dmitry <ribkatt@gmail.com>
2019-05-09 14:25:35 +0000

    implement rollup service

---
+++ rollup/service.js
@@ -0,0 +1,63 @@
+
+import {resolve, dirname} from 'path'
+import {rollup} from 'rollup'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+
+import taskService from '../lib/taskService'
+import babelConfig from './defaultBabelConfig'
+
+const babelPlugin = babel(babelConfig)
+const jsonPlugin = json({
+  preferConst: true,
+  indent: '  ',
+})
+
+export default taskService({
+  serviceName: 'rollup',
+  async task(ctx, bodyRaw) {
+    const body = Object.assign({}, {
+      files: [],
+      entry: 'repl.js',
+    }, bodyRaw)
+    const files = body.files
+    const filesByID = new Map()
+    for (const file of files) {
+      filesByID.set(file.name, file)
+    }
+    console.log('init rollup')
+    const build = await rollup({
+      input: body.entry,
+      plugins: [
+        {
+          resolveId(importee, importer) {
+            if (!importer) return importee;
+            if (importee[0] !== '.') return false;
+            const resolved = resolve(dirname(importer), importee).replace(/^\.\//, '');
+            if (filesByID.has(resolved)) return resolved;
+            const resolvedJS = `${resolved}.js`;
+            if (filesByID.has(resolvedJS)) return resolvedJS;
+            const resolvedJSON = `${resolved}.json`;
+            if (filesByID.has(resolvedJSON)) return resolvedJSON;
+            throw new Error(`Could not resolve '${importee}' from '${importer}'`);
+          },
+          load(id) {
+            return filesByID.get(id).code;
+          }
+				},
+        jsonPlugin,
+        babelPlugin,
+      ],
+    })
+    console.log('generate bundle')
+    const generated = await build.generate({
+      format: 'umd',
+      name: 'myBundle',
+      globals: {},
+    })
+    console.log('generated', generated)
+    return {
+      code: generated,
+    }
+  },
+})
